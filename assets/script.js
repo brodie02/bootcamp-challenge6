43d05e08e630a6892d87a0d0c88e3493797a4a18
brodie02 <marshallbrodie0@gmail.com>
2022-10-26 16:06:33 +1030

    Fetch geocode API

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,21 @@
 var apiKey = "cb85dae758eedd554f85531872892ce8" 
-var cityInput = document.querySelector("#city-input")
+var city = document.querySelector("#city-input").value
 
-function fetchCurrentWeatherAPI() {
+function fetchGeoCodeAPI(city) {
+    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey
+    fetch(url)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        //console.log(data);
+        fetchCurrentWeatherAPI(data[0].lat, data[0].lon)
+    })
+}
+
+function fetchCurrentWeatherAPI(lat, lon) {
     var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=" + apiKey
-    var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey
+    var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=d1acb13869b02cbe9450cb2a103eba92"
     fetch(oneCallUrl)
     .then(function(response) {
         return response.json()
@@ -25,8 +37,8 @@ function fetchFutureWeatherAPI() {
 }
 
 function init() {
-    fetchCurrentWeatherAPI()
-    fetchFutureWeatherAPI()
+    fetchGeoCodeAPI("Adelaide")
+    //fetchFutureWeatherAPI()
 }
 
 init()
