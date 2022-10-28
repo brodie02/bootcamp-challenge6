ed6750def5ba721c25681acefea01d982672ada9
brodie02 <marshallbrodie0@gmail.com>
2022-10-28 13:06:53 +1030

    Get current weather data

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var apiKey = "cb85dae758eedd554f85531872892ce8" 
+var apiKey = "47f166773e351368285402b79068ea73" 
 
 function getCityInput() {
     var city = document.getElementById("city-input").value
@@ -23,36 +23,21 @@ function fetchGeoCodeAPI(city) {
     })
     .then(function(data) {
         //console.log(data);
-        fetchCurrentWeatherAPI(data[0].lat, data[0].lon)
+        fetchWeatherAPI(data[0].lat, data[0].lon)
     })
 }
 
-function fetchCurrentWeatherAPI(lat, lon) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=" + apiKey
-    var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=d1acb13869b02cbe9450cb2a103eba92"
-    fetch(oneCallUrl)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data);
-    })
-}
-
-function fetchFutureWeatherAPI() {
-    var url = "https://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=" + apiKey
+function fetchWeatherAPI(lat, lon) {
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + apiKey
     fetch(url)
     .then(function(response) {
         return response.json()
     })
     .then(function(data) {
         console.log(data);
+        console.log(data.current.temp + "°C");
+        console.log(data.current.wind_speed + " " + "KMPH");
+        console.log(data.current.humidity + "%");
+        console.log(data.current.uvi);
     })
 }
-
-
-function init() {
-    //fetchFutureWeatherAPI()
-}
-
-init()
